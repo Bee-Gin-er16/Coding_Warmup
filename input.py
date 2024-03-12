bdf8ad5d9b7cac10ca32787feafe1d6c6b8ef98f
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2024-03-12 10:15:15 +0800

    March 12 Warmup
    Finished 1-10 Exercises for Input/Output

---
+++ input.py
@@ -0,0 +1,56 @@
+#OP1
+# num1 = int(input("Enter the first number:"))
+# num2 = int(input("Enter the second number:"))
+# print(f"Product of {num1} and {num2} is: {num1+num2}")
+
+#OP2
+print('Name','Is','James', sep="**")
+
+#Op3
+print("%o" % 8)
+
+#OP4
+print("%.2f" % 458.541315)
+
+#OP5
+# numbers = []
+# for num in range(5):
+#     num = float(input(f"Enter the {num}st/th number:"))
+#     numbers.append(num)
+# print(numbers)
+
+print("\n")
+
+#OP6
+# with open("text.txt", "r") as fp:
+#     lines = fp.readlines()
+# with open("new_file.txt", "w") as fp:
+#     count = 0
+#     for line in lines:
+#         if count == 4:
+#             count += 1
+#             continue
+#         else:
+#             fp.write(line)
+#         count += 1
+
+#OP7
+# str1, str2, str3 = input("Enter three strings:").split()
+# print(f"Str1: {str1} Str2: {str2} Str43 {str3}")
+        
+#OP8
+totalMoney = 1000
+quantity = 3
+price = 450
+print(f"I have {totalMoney} dollars so I can buy {quantity} football for {price:.2f} dollars. \n")
+
+#Op9
+import os
+size = os.stat("text.txt").st_size
+print('\nfile is empty\n') if size == 0 else print('file is not empty\n')
+
+#Op10
+with open('text.txt', "r") as fp:
+    lines = fp.readlines()
+    print(lines[2])
+    fp.close()
