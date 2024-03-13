3a15b89fd14223f30ddb464151132cf6352ed483
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2024-03-13 11:56:42 +0800

    loop exercise completed

--- loop.py
+++ loop.py
@@ -65,26 +65,76 @@ for i in range(5):
     print(i, end=", ")
 else:
     print(" Done!")
+print(" ")
 
 #Op11
-
+start = 25
+end = 50
+print(f"Prime numbers between {start} and {end} are the ff:")
+for nums in range(start, end+1):
+    if nums > 1:
+        for itr in range(2, nums):
+            if(nums % itr) == 0:
+                break
+        else: 
+            print(nums, end=", ")
+print("\n")
 
 #Op12
-
+fib1 = 0
+fib2 = 1
+for iter in range(10):
+    print(fib1, end=", ")
+    output = fib1 + fib2
+    fib1 = fib2
+    fib2 = output
+print("\n")
 
 #Op13
-
+result = 1
+for fact in (range(1,5+1)):
+    result *= fact
+print(result)
+print(" ")
 
 #Op14
-
+num = 76542
+reverse_number = 0
+print("Given Number ", num)
+while num > 0:
+    reminder = num % 10
+    reverse_number = (reverse_number * 10) + reminder
+    num = num // 10
+print(f"Reverse Number: {reverse_number}\n")
 
 #Op15
-
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+for ind in range(len(my_list)):
+    if ind % 2 != 0: print(my_list[ind], end=", ")
+print("\n")
 
 #Op16
+for integ in range(6):
+    print(f"Current number is :{integ+1} and the cube is :{(integ+1)**3}")
+print(" ")
 
 #Op17
+giv_num = num = 2
+limit = 5
+res = 0
+for ind in range(limit):
+    res += num
+    num = (num * 10) + giv_num
+print(f"{res}\n") 
 
 #Op18
-
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
+for i in range(rows, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print("\r")
     
