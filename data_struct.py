5c0628de7f7c8e7a0ce1a94b16e0e0a0fa682a7a
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2024-03-26 21:21:27 +0800

    Done and never again with Data structs
    I remembered why I hate data structures. It's like it boils down to the basics of an existing data type. This isn't even Java and it's still hard. All I did was follow the hints.

--- data_struct.py
+++ data_struct.py
@@ -72,4 +72,84 @@ print("Second List ", second_list)
 result = zip(first_list, second_list)
 result_set = set(result)
 print(result_set)
-    
+
+print("\n")
+#Op6
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+intersection = first_set.intersection(second_set)
+print("Intersection is ", intersection)
+for item in intersection:
+    first_set.remove(item)
+
+print("First Set after removing common element ", first_set)
+
+print("\n")
+#Op7
+first_set = {57, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+print("First set is subset of second set -", first_set.issubset(second_set))
+print("Second set is subset of First set - ", second_set.issubset(first_set))
+
+print("First set is Super set of second set - ", first_set.issuperset(second_set))
+print("Second set is Super set of First set - ", second_set.issuperset(first_set))
+
+if first_set.issubset(second_set):
+    first_set.clear()
+
+if second_set.issubset(first_set):
+    second_set.clear()
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+print("\n")
+#Op8
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon': 47, 'Emma': 69, 'Kelly': 76, 'Jason': 97}
+
+print("List:", roll_number)
+print("Dictionary:", sample_dict)
+
+# create new list
+roll_number[:] = [item for item in roll_number if item in sample_dict.values()]
+print("after removing unwanted elements from list:", roll_number)
+
+print("\n")
+#Op9
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53,
+         'july': 54, 'Aug': 44, 'Sept': 54}
+
+print("Dictionary's values - ", speed.values())
+
+speed_list = list()
+
+# iterate dict values
+for val in speed.values():
+    # check if value not present in a list
+    if val not in speed_list:
+        speed_list.append(val)
+print("unique list", speed_list)
+
+print("\n")
+#Op10
+sample_list = [87, 52, 44, 53, 54, 87, 52, 53]
+
+print("Original list", sample_list)
+
+sample_list = list(set(sample_list))
+print("unique list", sample_list)
+
+t = tuple(sample_list)
+print("tuple ", t)
+
+print("Minimum number is: ", min(t))
+print("Maximum number is: ", max(t))
