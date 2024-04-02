46b0d221cc5d320defeb669caf9c4e3893de99a2
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2024-04-02 13:46:27 +0800

    Finished list data struct and it's functions

--- listfxn.py
+++ listfxn.py
@@ -27,7 +27,31 @@ res = [f"{x} {y}" for x,y in zip(list1, list2[::-1])]
 print(res)
 
 #OP6
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+res = list(filter(None, list1))
+print(res)
+
 #OP7
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+list1[2][2].append(7000)
+print(list1)
+
 #OP8
+list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+sub_list = ["h", "i", "j"]
+list1[2][1][2].extend(sub_list)
+print(list1)
+
 #OP9
+list1 = [5, 10, 15, 20, 25, 50, 20]
+list1[list1.index(20)]= 200
+print(list1)
+
 #OP10
+list1 = [5, 20, 15, 20, 25, 50, 20]
+def remove_value(sample_list, val):
+    return [i for i in sample_list if i != val]
+
+res = remove_value(list1, 20)
+print(res)
+
